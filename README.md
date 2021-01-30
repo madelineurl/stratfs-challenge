2ae608d0a9dec774729d273f463c146ced320e74
Maddie Higgins <maddiehiggins9@gmail.com>
2021-01-29 19:43:29 -0600

    fetch data and render in a table

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Boilerplate
-This is a barebones PERN boilermaker with update-to-date dependencies and a basic full-stack configuration.
+# Strategic Financial Solutions Challenge
+More info to come!
--- client/app.js
+++ client/app.js
@@ -1,8 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const App = () => {
+  const [clientData, setClientData] = useState([]);
+  useEffect(() => {
+    async function getData() {
+      try {
+        const { data } = await axios.get('https://raw.githubusercontent.com/StrategicFS/Recruitment/master/data.json');
+        setClientData(data);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+
+    getData();
+  }, []);
+
+
   return (
-    <div>Hello world!</div>
+    <>
+      <table>
+        <tbody>
+          <tr>
+            <th>Creditor</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Min Pay %</th>
+            <th>Balance</th>
+          </tr>
+          {
+            clientData.map(client => (
+              <tr key={client.id}>
+                <td>{client.creditorName}</td>
+                <td>{client.firstName}</td>
+                <td>{client.lastName}</td>
+                <td>{client.minPaymentPercentage}%</td>
+                <td>{client.balance}.00</td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
+    </>
   );
 };
 
